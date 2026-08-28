b6d91e71a77a00e324f5f1cd6e0c85250630fd97
macreat <mateomacreat@gmail.com>
2026-08-27 19:45:28 -0500

    fix(notifications): send Twilio order notifications to +573245710972

--- app/microservices/notifications/src/config.py
+++ app/microservices/notifications/src/config.py
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     twilio_account_sid: str = ""
     twilio_auth_token: str = ""
     twilio_from: str = "whatsapp:+14155238886"
-    twilio_to: str = "+15715683120"
+    twilio_to: str = "+573245710972"
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
